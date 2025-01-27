2e223201c8ec862406145d9348357a061ea306c9
ahushv3432 <andreyriabov67@gmail.com>
2025-01-27 21:06:46 +0200

    Add files via upload

---
+++ config.py
@@ -0,0 +1,8 @@
+# this code written by @slivmens
+
+api_id = 123 # from https://my.telegram.org/
+api_hash = "123" # from my.telegram.org
+admin_id = 1404656666 # from @userinfobot
+token = "123" # from botfather
+
+# this code written by @slivmens
